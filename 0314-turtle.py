15d790d8a52c08539cd6fc515455adcb5f99f612
챙 <>
2025-03-14 15:45:16 +0900

    update 0314

---
+++ 0314-turtle.py
@@ -0,0 +1,17 @@
+import turtle
+
+turtle.forward(100)
+turtle.right(144)
+
+turtle.forward(100)
+turtle.right(144)
+
+turtle.forward(100)
+turtle.right(144)
+
+turtle.forward(100)
+turtle.right(144)
+
+turtle.forward(100)
+
+turtle.done()
