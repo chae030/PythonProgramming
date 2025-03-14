873446942a6948ddcbf5fde5233a68af59a9ced2
챙 <>
2025-03-14 15:25:33 +0900

    update

---
+++ 0314.py
@@ -0,0 +1,7 @@
+num1 = int(input("number 0:"))
+num2 = int(input("number 1:"))
+
+print(num1, " + ", num2, " = ", num1+num2)
+print(num1, " - ", num2, " = ", num1-num2)
+print(num1, " * ", num2, " = ", num1*num2)
+print(num1, " / ", num2, " = ", num1/num2)
