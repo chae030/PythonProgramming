4f51106b631bab551f1e231c8c5a62c773c5512e
챙 <cindy03@tukorea.ac.kr>
2025-04-04 11:41:56 +0900

    update0404

---
+++ 0404-3-5.py
@@ -0,0 +1,7 @@
+num = int(input("Enter an integer: "))
+print("- partition -")
+
+if num % 3 == 0 :
+    print("This number is a multiple of 3.")
+if num % 5 == 0 :
+    print("This number is a multiple of 5.")
