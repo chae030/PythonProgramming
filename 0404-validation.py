971be4ee03b6ab6f4f45c2096cd0c8bd8a7fde17
챙 <cindy03@tukorea.ac.kr>
2025-04-04 12:00:49 +0900

    update0404

--- 0404-validation.py
+++ 0404-validation.py
@@ -1,16 +1,14 @@
-i = int(input("Enter an integer (0-255): "))
-j = int(input("Enter bit position (0-7): "))
-print("- partition -")
+k = 1
+i = int(input("Enter an integer (0-255):"))
+j = int(input("Enter bit position (0-7):"))
 
 if i < 0 or i > 255 :
     print("Error : number must be between 0 and 255")
-
-if j < 0 or j > 7 :
-    print("Error : numbr must be between 0 and 7")
-
-k = 1
-k = k << j
-
-print(f"{i:08b} : binary representation of {i}")
-print(f"{k:08b} : bit mask position {j}")
-print(f"Is bit of {i} at position {j} on? {i&k>0}")
+elif j < 0 or j > 7 :
+    print("Error : number must be between 0 and 7")
+else :
+    print("- partition -")
+    k = k << j
+    print(f"{i:08b} : binary representation of {i}")
+    print(f"{k:08b} : bit mask for position {j}")
+    print(f"Is bit of {i} at position {j} on? {i&k>0}")
